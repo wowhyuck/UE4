e45ad99ec0ff1461a2e71c2e820fd3772186be85
Joohyuck <joohyuck.seo@gmail.com>
2021-10-08 19:31:29 +0900

    블루프린트 클래스

--- Source/TestUnrealEngine/MyGameModeBase.cpp
+++ Source/TestUnrealEngine/MyGameModeBase.cpp
@@ -6,5 +6,12 @@
 
 AMyGameModeBase::AMyGameModeBase()
 {
-	DefaultPawnClass = AMyCharacter::StaticClass();
+	//DefaultPawnClass = AMyCharacter::StaticClass();
+
+	static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("Blueprint'/Game/Blueprints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+	{
+		DefaultPawnClass = BP_Char.Class;
+	}
 }
