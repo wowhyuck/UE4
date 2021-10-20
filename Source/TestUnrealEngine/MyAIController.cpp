0a07948215e18359214e0a6c608d5b0d86fcdb40
Joohyuck <joohyuck.seo@gmail.com>
2021-10-21 00:38:47 +0900

    AI Controller

---
+++ Source/TestUnrealEngine/MyAIController.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+#include "NavigationSystem.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+AMyAIController::AMyAIController()
+{
+
+}
+
+void AMyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AMyAIController::RandomMove, 3.f, true);
+
+
+}
+
+void AMyAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+}
+
+void AMyAIController::RandomMove()
+{
+	auto CurrentPawn = GetPawn();
+
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+
+	if (NavSystem == nullptr)
+		return;
+
+	FNavLocation RandomLocation;
+
+	if (NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.f, RandomLocation))
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, RandomLocation);
+	}
+}
--- Source/TestUnrealEngine/MyCharacter.cpp
+++ Source/TestUnrealEngine/MyCharacter.cpp
@@ -10,6 +10,7 @@
 #include "MyStatComponent.h"
 #include "Components/WidgetComponent.h"
 #include "MyCharacterWidget.h"
+#include "MyAIController.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
@@ -49,6 +50,9 @@ AMyCharacter::AMyCharacter()
 		HpBar->SetWidgetClass(UW.Class);
 		HpBar->SetDrawSize(FVector2D(200.f, 50.f));
 	}
+
+	AIControllerClass = AMyAIController::StaticClass();
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 
 }
 
